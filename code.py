83c6fec7faca7d523d6998d8f45fb9253902173c
Yash Tanwar <yashtanwar11092005@gmail.com>
2025-12-30 22:46:17 +0530

    dataset folder creation code done

--- code.py
+++ code.py
@@ -37,7 +37,23 @@ val_df,test_df=train_test_split(
     temp_df,test_size=0.50,stratify=temp_df['dx'],random_state=42
     )
 
+#copying the images fro, source to output folder
 def copy_images(dataframes,split_name):
     for _, row in tqdm (dataframes.iterrows(),total=len(dataframes)):
         img_name=row["image_id"]+".jpg"
+        label = row ['dx']
         
+        src_path = os.path.join(image1_dir,img_name)
+        if not os.path.exists(src_path):
+            src_path = os.path.join(image2_dir,img_name)
+        
+        dest_path=os.path.join(output_dir,split_name,label,img_name)
+        shutil.copyfile(src_path,dest_path)
+
+copy_images(train_df, "train")
+copy_images(val_df, "val")
+copy_images(test_df, "test")
+
+print("Output folder created successfully.")
+
+#till here code needs to be run once for folder creation
