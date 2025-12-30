f896b58f79c10ac366d7eb4b0f8bc1f4448dad32
Yash Tanwar <yashtanwar11092005@gmail.com>
2025-12-30 23:31:03 +0530

    dataset added

--- detection_disease_model.py
+++ detection_disease_model.py
@@ -4,6 +4,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch 
 import tensorflow as tf 
+import sklearn
+from sklearn.metrics import classification_report,f1_score,confusion_matrix,accuracy_score,precision_score,recall_score
+
+#device configuration
+device= torch.device('cuda' if torch.cuda.is_available() else'cpu')
+print(f'using device:',device)
 
 #dataset directories 
+test_split=r'C:\Users\Hp\Documents\dataset folders\skin disease dataset\6GB HAM10000 DATA\output_folder\test'
+train_split=r'C:\Users\Hp\Documents\dataset folders\skin disease dataset\6GB HAM10000 DATA\output_folder\train'
+val_split=r'C:\Users\Hp\Documents\dataset folders\skin disease dataset\6GB HAM10000 DATA\output_folder\val'
 
