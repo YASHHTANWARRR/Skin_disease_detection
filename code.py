d22b56a81b1df70d86f7ab7547e6b3811ed48b8b
Yash Tanwar <yashtanwar11092005@gmail.com>
2025-12-30 19:29:20 +0530

    libraries and dataset added

---
+++ code.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+import numpy as np 
+import pandas as pd
+import torch 
+import matplotlib.pyplot as plt 
+import tensorflow as tf
+
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+
+#directories
+basic_dir = "C:\\Users\\Hp\\Documents\\dataset folders\\skin disease dataset\\6GB HAM10000 DATA\\HAM10000_metadata.csv"
+image1_dir = "C:\\Users\\Hp\\Documents\\dataset folders\\skin disease dataset\\6GB HAM10000 DATA\\HAM10000_images_part_1"
+image2_dir = "C:\\Users\\Hp\\Documents\\dataset folders\\skin disease dataset\\6GB HAM10000 DATA\\HAM10000_images_part_2"
+
+
+
+
+
+
+
